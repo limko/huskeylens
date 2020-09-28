cd5f7014efc3799ff9ccf368cd11f72dff397796
MILKA <anthonylimko@gmail.com>
2020-09-28 11:30:33 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-let ID = 0
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 basic.forever(function () {
     huskylens.request()
-    ID = huskylens.readBox_s(Content3.ID)
+    if (huskylens.isLearned(1)) {
+        if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        	
+        }
+    }
 })
