2e347f874059ec257219ec2243f45181ce301bcf
MILKA <anthonylimko@gmail.com>
2020-09-28 11:51:58 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,35 @@
+function TakeNote () {
+    ID = huskylens.readBox_s(Content3.ID)
+    if (0 == 0) {
+    	
+    }
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+    if (true) {
+    	
+    }
+}
+let ID = 0
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 basic.forever(function () {
     huskylens.request()
     if (huskylens.isLearned(1)) {
         if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        	
+            TakeNote()
         }
     }
 })
