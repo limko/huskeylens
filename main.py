b44c62a4af7c7a19cc32c5d0f375781503ec9c7d
MILKA <anthonylimko@gmail.com>
2020-09-29 01:20:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+def TakeNote():
+    global ID
+    ID = huskylens.readBox_s(Content3.ID)
+    if ID == 1:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.BLUE)
+    if ID == 2:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.RED)
+    if ID == 3:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.GREEN)
+    if ID == 4:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.YELLOW)
+    if ID == 5:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.CYAN)
+    if ID == 6:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.PINK)
+    if ID == 7:
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
+ID = 0
 huskylens.init_i2c()
+huskylens.init_mode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 
 def on_forever():
-    pass
+    huskylens.request()
+    if huskylens.is_learned(1):
+        if huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+            TakeNote()
 basic.forever(on_forever)
